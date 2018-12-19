8caad36c5218d55fe5a98b9c69983d5456a4fc94
nnamdi16 <nwabuokeinnamdi@gmail.com>
2018-12-19 16:51:27 +0100

    Update the test case for tolower method

--- stringClass.spec.js
+++ stringClass.spec.js
@@ -54,20 +54,20 @@ describe("String class Extension", function() {
   });
 
   describe("Returns String with all characters in uppercase", function() {
-    it("should return all characters to be uppercase when all characters are lowercase", function() {
-      expect('nnamdi'.toUpper()).toBe('NNAMDI');
+    it("should return all characters to be lowercase when all characters are lowercase", function() {
+      expect('NNAMDI'.tolower()).toBe('nnamdi');
     });
 
-    it("should return all characters to be uppercase when characters are both uppercase and lowercase", function() {
-      expect('nnAmDi'.toUpper()).toBe('NNAMDI');
+    it("should return all characters to be lowercase when characters are both uppercase and lowercase", function() {
+      expect('nnAmDi'.tolower()).toBe('nnamdi');
     });
 
     it("should return empty string on empty string input", function() {
-      expect(''.toUpper()).toBe('');
+      expect(''.tolower()).toBe('');
     });
 
-    it("should return capital letters if all characters are uppercase", function() {
-      expect('NNAMDI'.toUpper()).toBe('NNAMDI');
+    it("should return lowercase letters if all characters are lowercase", function() {
+      expect('nnamdi'.tolower()).toBe('nnamdi');
     });
 
    
