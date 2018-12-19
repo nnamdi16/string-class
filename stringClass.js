bfcde2af8b0b65d8b417f022e2ff4871f9ae0c74
nnamdi16 <nwabuokeinnamdi@gmail.com>
2018-12-19 15:11:03 +0100

    All test passed after function creation and modifications

--- stringClass.js
+++ stringClass.js
@@ -1,8 +1,8 @@
 //Creating a constr 
 String.prototype.hasVowels = function() {
-	// let word = /[aeiou]/i;
-	// let newWord = Object.values(this).join('');
-	// return word.test(newWord) ? true : false;
+	let word = /[aeiou]/i;
+	let newWord = Object.values(this).join('');
+	return word.test(newWord) ? true : false;
 }
 
 
