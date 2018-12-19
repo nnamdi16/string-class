aeda7b1f85ff4f6e8f3bfa6938ffc474c445ea4b
nnamdi16 <nwabuokeinnamdi@gmail.com>
2018-12-19 16:45:51 +0100

    Update the string method creating a toUpper method for converting strings to uppercase

--- stringClass.js
+++ stringClass.js
@@ -1,8 +1,18 @@
-//Creating a constr 
+//Creating a method that checks for vowels 
 String.prototype.hasVowels = function() {
-	let word = /[aeiou]/i;
-	let newWord = Object.values(this).join('');
-	return word.test(newWord) ? true : false;
+	let regexTest = /[aeiou]/i;
+	let word = Object.values(this).join('');
+	return regexTest.test(word) ? true : false;
+}
+
+//Method for creating upperCase
+String.prototype.toUpper = function() {
+	let upperRegex = /[a-z]/;
+	let output = '';
+	for(let index = 0; index < this.length; index++){
+		output += this[index].replace(upperRegex, String.fromCharCode(this[index].charCodeAt(0)-32));
+	}
+	return output;
 }
 
 
